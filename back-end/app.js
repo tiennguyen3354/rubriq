f2c5ed3d58e3cf6b6b809bdf2ebf258d2bfd4dd3
mehaksaini <saini.mehak@student.greenriver.edu>
2025-01-28 11:28:32 -0800

    app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,8 @@
 import express from 'express'; 
 
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import uploadRoutes from './routes/uploadRoutes.js';
 const app = express(); 
 
 
@@ -8,10 +10,25 @@ const app = express();
 app.use(express.json()); 
 
 
-app.get("/", (req, res) => {
-    res.send("Server is running! testing ");
-  });
+// Get __dirname  for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, '../front-end')));
+
+// Serve static files from the `uploads` folder (to make the files publicly accessible)
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+// Serve `view.html` for the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front-end', 'view.html'));
+});
+
+// Use the upload routes for handling file uploads
+app.use(uploadRoutes);
+
 
 // listenings 
-app.listen(4242, () => console.log(`Server stared on port 4242`)); 
+app.listen(4242, () => console.log(` http://localhost:4242`)); 
 
